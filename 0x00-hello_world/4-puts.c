ae4086c63499103b1ab36ec5cc6204a029c07267
Gracious <graciousnguwo56@gmail.com>
2021-06-17 21:25:09 +0200

    a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-  * main -print text
+  * main - prints text
   *
   * Return: 0 (Success)
   */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+    return (0);
 }
