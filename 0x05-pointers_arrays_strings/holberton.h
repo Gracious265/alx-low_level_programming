c8c8f580ea6529569d6916c63bbf77f23a5960fc
Gracious Nguwo <graciousnguwo2020@gmail.com>
2021-06-29 22:39:29 +0200

    pointers,arrays & strings

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,3 +12,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
+
+
