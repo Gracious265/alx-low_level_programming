7832f03ee418d0fe5ee018f3f200c79f3d7921c4
Gracious Nguwo <graciousnguwo2020@gmail.com>
2021-07-16 22:41:38 +0200

    modificed holberton

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -30,5 +30,9 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
 
 #endif /* _HOLBERTON_H_ */
