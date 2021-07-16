3ed610735067c7c4fb7b74954fe1c07355291cd8
Gracious Nguwo <graciousnguwo2020@gmail.com>
2021-07-16 22:06:42 +0200

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,38 @@
-#include <stdlib.h>
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
+ **string_nconcat - a function that concatenates two strings.
+ *@s1 : pointer to string
+ *@s2 : pointer to string
+ *@n : integer
+ *Return: pointer to string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *strconc;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
-if (n < len2)
-strconc = malloc(sizeof(char) * (len1 + n + 1));
-else
-strconc = malloc(sizeof(char) * (len1 + len2 + 1));
-if (!strconc)
-return (NULL);
-while (i < len1)
-{
-strconc[i] = s1[i];
-i++;
-}
-while (i < (len1 + n))
-{
-strconc[i] = s2[j];
-i++;
-j++;
-}
-strconc[i] = '\0';
-return (strconc);
+
+
+	unsigned int x1, x2, i, j;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (x1 = 0; s1[x1] != '\0'; x1++)
+	;
+	for (x2 = 0; s2[x2] != '\0'; x2++)
+	;
+	if (n >= x2)
+		n = x2;
+	s = malloc(sizeof(char) * (x1 + n + 1));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = x1; j < x1 + n; j++)
+		s[j] = s2[j - x1];
+	s[j] = '\0';
+	return (s);
 }
