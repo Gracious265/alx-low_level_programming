7554412e0d9f42577929582b8aeb557d1be6c9f4
Gracious Nguwo <graciousnguwo2020@gmail.com>
2021-07-16 22:01:59 +0200

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,40 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1:stirng 1
- * @s2:string 2
- * @n: n bytesofstring 2
- * Return: pointer
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *ptr, *empt;
-  unsigned int i, k;
+char *strconc;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-  empt = "";
-  if (s1 == NULL)
-    s1 = empt;
-  if (s2 == NULL)
-    s2 = empt;
-  for (i = 0; s1[i] != '\0'; i++)
-    ;
-  ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
-  if (ptr == NULL)
-    return (NULL);
-  for (i = 0; s1[i] != '\0'; i++)
-    ptr[i] = s1[i];
-  for (k = 0; s2[k] != '\0' && k < n; k++, i++)
-    ptr[i] = s2[k];
-  ptr[i] = '\0';
-  return (ptr);
+while (s1 && s1[len1])
+len1++;
+while (s2 && s2[len2])
+len2++;
+if (n < len2)
+strconc = malloc(sizeof(char) * (len1 + n + 1));
+else
+strconc = malloc(sizeof(char) * (len1 + len2 + 1));
+if (!strconc)
+return (NULL);
+while (i < len1)
+{
+strconc[i] = s1[i];
+i++;
+}
+while (i < (len1 + n))
+{
+strconc[i] = s2[j];
+i++;
+j++;
+}
+strconc[i] = '\0';
+return (strconc);
 }
